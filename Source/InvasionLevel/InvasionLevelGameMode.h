cf51aaadacc5c689ce3ca5af3d5a85d9ccfe0c2c
Wamger <wolfripperbgbg@gmail.com>
2017-06-21 19:20:06 +0300

    Uploading base files

---
+++ Source/InvasionLevel/InvasionLevelGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InvasionLevelGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AInvasionLevelGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AInvasionLevelGameMode();
+};
+
+
+
